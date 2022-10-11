f607cbe2cc639fb8ec9f34d36169fb442f8de724
dldntjr0516 <dldntjr0516@gmail.com>
2022-10-12 00:28:21 +0900

    6주차 - 순수 자바로 회원 도메인, 주문과 할인 도메일 구현

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 예외 처리까지 해주어야 하지만, 에제에서는 간단하게 구현.
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
