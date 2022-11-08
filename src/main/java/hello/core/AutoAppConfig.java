2fff88ca867fc0da178b1bfe350d5d8c8f66be0e
dldntjr0516 <dldntjr0516@gmail.com>
2022-11-08 21:57:20 +0900

    10주차 - 싱글톤 방식의 주의점, @Configuration과 싱글톤 그리고 바이트 코드, @ComponentScan, @Component, @Autowired

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+// @Configuration의 내부를 들여다보면 @Component가 존재하기 때문에 자동 스캔의 대상이 됨.
+// 예제를 안전하게 유지하기 위해 excludeFilters를 이용하여 AppConfig, TestConfig를 제외.
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.HashMap;
 import java.util.Map;
 
 // 예외 처리까지 해주어야 하지만, 에제에서는 간단하게 구현.
+@Component
 public class MemoryMemberRepository implements MemberRepository {
     private static Map<Long, Member> store = new HashMap<>();
 
